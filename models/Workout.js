dd1c4821485712c1cd70da871f0e56157d6b0965
Jeckfeldt <jeckfeldt123@gmail.com>
2021-07-13 18:04:16 -0700

    code

---
+++ models/Workout.js
@@ -0,0 +1,45 @@
+const {Schema, Model} = require('mongoose')
+
+const Workout = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+      },
+      duration: Number,
+      weight: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      distance: {
+        type: Number,
+        default: 0
+      }
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
+})
+
+const Workout = mongoose.model("Workout", Workout);
+
+module.exports = Workout
